33eb8a41b15e75409fdfd88b61fa797d1a7c342d
= <=>
2017-09-11 10:10:29 +0800

    object

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require("gulp");
+var cssClean = require("gulp-clean-css");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
+gulp.task("cleanCss",function(){
+	gulp.src("css/*.css")
+	    .pipe(cssClean())
+	    .pipe(gulp.dest("build/css"))
+})
+gulp.task("cleanJs",function(){
+	gulp.src("js/*.js")
+	    .pipe(uglify())
+	    .pipe(gulp.dest("build/js"))
+})
